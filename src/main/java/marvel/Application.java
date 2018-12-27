aac4fcd9b33a3298810625feb99eac4318c2494d
Thomas Johander <thomas.johnader@target.com>
2018-12-27 14:37:17 -0600

    Updated application.java

--- src/main/java/marvel/Application.java
+++ src/main/java/marvel/Application.java
@@ -28,9 +28,9 @@ public class Application
 
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
-//			for (String beanName : beanNames) {
-//				System.out.println(beanName);
-//			}
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
 
 		};
 	}
